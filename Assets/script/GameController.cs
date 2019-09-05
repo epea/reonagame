ac2b0bbff52cec7886aaabef26b7be76a3588e1e
Yoshitake Kitamura <kitamura@epea.co.jp>
2019-09-05 11:22:04 +0900

    initial

---
+++ Assets/script/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public UnityEngine.UI.Text scoreLabelA;
+
+    public GameObject winnerLabelObject;
+
+    public void Update ()
+    {
+        int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		scoreLabelA.text = count.ToString ();
+
+        if(count == 0){
+            winnerLabelObject.SetActive (true);
+        }
+    }
+}
